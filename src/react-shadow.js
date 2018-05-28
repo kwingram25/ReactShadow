dc33af12cb039f2e5d1e0ae7e3bf6409d5c3ab35
kwingram25 <kwingram25@gmail.com>
2018-05-27 20:49:07 -0700

    fix

--- src/react-shadow.js
+++ src/react-shadow.js
@@ -202,6 +202,7 @@ export const withContext = contextTypes => {
             // Updates consist of simply rendering the container element into the shadow root again, as
             // the `this.wrapContainer()` element contains the passed in component's children.
             render(this.wrapContainer(), this.state.root);
+            retargetEvents(this.state.root);
 
         }
 
